fa92845955b057bcc8c658b4e3363a71e636e9e6
Simon Krull <krull@punkt.de>
2022-09-25 13:25:00 +0200

    TASK: initial commit

---
+++ Java/hello-world/src/BirthdayProgram/birthdayProgram.java
@@ -0,0 +1,32 @@
+package BirthdayProgram;
+
+import java.util.Scanner;
+
+public class birthdayProgram {
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String name;
+		System.out.println("Please enter your name:");
+		name = sc.next();
+
+		// Calculates the year of birth and asks you if you already had birthday this year
+		int age;
+		int thisYear = 2022;
+		int birthYear = 2001;
+		System.out.println("Please enter your year of birth:");
+		birthYear = sc.nextInt();
+
+		System.out.println("Do you already had birthday this year? 1: Yes, 2: no");
+		int choice = sc.nextInt();
+		
+		if (choice == 1) {
+			age = thisYear - birthYear;
+		} else {
+			age = thisYear - birthYear - 1;
+		}
+		
+		System.out.println("Hello " + name + "," + " you are " + age +  " years old");
+	}
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# School Projects :school:
+
+Here is a list of projects from the school in different programming languages! :)
+
+Contributions are very welcome! :tada:
