db19c866f30adfeb2c553125591c375eac7d2577
Simon Krull <simondotunix@gmail.com>
2022-12-13 10:54:38 +0100

    TASK: implement functionality to restart the program

--- Python/birthday-program/birthday-program.py
+++ Python/birthday-program/birthday-program.py
@@ -1,15 +1,21 @@
-class BirthdayProgram:
+import os
+import sys
 
-    thisYear = 2022
+thisYear = int(input("Please enter the current year "))
+name = input("Please enter your name: ")
+age = int(input("Please enter your year of birth: "))
+choice = str(input("Do you already had birthday this year? yes/no "))
 
-    name = input("Please enter your name: ")
-    age = int(input("Please enter your year of birth: "))
-    choice = int(input("Do you already had birthday this year? 1: Yes, 2: no "))
+if choice == "yes":
+    age = thisYear - age
+    print("Hello " + name + "," + " you are " + str(age) + " years old")
+else:
+    age = thisYear - age - 1
+    print("Hello " + name + "," + " you are still " + str(age) + " years old")
 
-    if choice == 1:
-        age = thisYear - age
-        print("Hello " + name + "," + " you are " + str(age) + " years old")
-    else:
-        age = thisYear - age - 1
-        print("Hello " + name + "," + " you are still " + str(age) + " years old")
+askAgainMessage = str(input("Would you like to ask another person? yes/no "))
 
+if askAgainMessage == "yes":
+    os.execv(sys.executable, ['python'] + sys.argv)
+else:
+    print("Good Bye!")
